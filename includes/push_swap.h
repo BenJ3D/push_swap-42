0623c155396352fb650b05bf942db5ac513c25b2
BenJ3D <benjamin@ben3d.fr>
2022-05-04 16:06:59 +0200

    bug syntx

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: bducrocq <bducrocq@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 18:51:25 by bducrocq          #+#    #+#             */
-/*   Updated: 2022/05/04 15:13:18 by bducrocq         ###   ########.fr       */
+/*   Updated: 2022/05/04 15:48:21 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,24 +38,8 @@ typedef struct s_list
 {
 	int				content;
 	struct s_list	*next;
-	struct s_list	*prev;
+	int				index;
 }					t_list;
-
-// typedef struct s_element t_element;
-
-// struct s_element
-// {
-// 	int				nbr;
-// 	t_element		*prev;
-// 	t_element		*next;
-// };
-
-// typedef struct s_lst
-// {
-// 	t_element		*first;
-	
-// }		t_lst;
-
 
 /****----------- struct -----------****/
 
@@ -64,6 +48,7 @@ typedef struct s_data
 	t_list			*stacka;
 	t_list			*stackb;
 	va_list			ap;
+	int				test;
 	enum e_error	error;
 }				t_data;
 
@@ -80,22 +65,12 @@ int		check_arg_is_valid(int ac, char **av);
 
 /******-------------- my_lst function --------------******/
 
-// t_lst	*lst_init(int);
-// void	lst_add(t_lst *liste, int new_nbr);
-// void	lst_del(t_lst *list);
 void	ft_lstprint(t_list *list);
-// void	lst_add_at_address(t_lst *list, int fnbr, int newnbr);
 
 //chained list
 t_list	*ft_lstnew(int content);
 void	ft_lstadd_front(t_list **alst, t_list *new);
-// int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **alst, t_list *new);
-//void	ft_lstdelone(t_list *lst, void (*del)(void *));
-//void	ft_lstclear(t_list **lst, void (*del)(void *));
-// void	ft_lstiter(t_list *lst, void (*f)(void *));
-// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-// char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,55 +6,18 @@
 /*   By: bducrocq <bducrocq@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 12:01:39 by bducrocq          #+#    #+#             */
-/*   Updated: 2022/05/04 15:17:41 by bducrocq         ###   ########.fr       */
+/*   Updated: 2022/05/04 16:04:18 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int stack_a_b(t_data data)
-{
-	return (0);
-}
-int testlst(t_list *list) //FIXME:
-{
-	t_list	**tmp;
-	
-	list = NULL;
-	ft_lstadd_back(&list, ft_lstnew(1));
-	ft_lstadd_back(&list, ft_lstnew(2));
-	ft_lstadd_back(&list, ft_lstnew(3));
-	 //printf lst
-	ft_lstprint(list);
-
-	tmp = &list;
-	//avance dans lst via tmp
-	tmp = &list->next->next;
-	ft_lstprint(list);
-	ft_lstprint(*tmp);
-	
-	//ajout element a cet endroit
-	ft_lstadd_front(tmp, ft_lstnew(42));
-	ft_lstadd_back(tmp, ft_lstnew(101));
-	ft_lstprint(list);
-	ft_lstprint(*tmp);
-	
-	return (0);
-}
-
 int	main(int ac, char **av)
 {
-	t_data data;
-	t_list	*tmp;
+	int ctn;
+	
+	ctn = 15;
 	
 	check_arg_is_valid(ac, av);
-
-	testlst(data.stacka);
-	// while(data.stacka) //TODO: essayer de free toute la chaine
-	// {
-	// 	tmp = data.stacka;
-	// 	data.stacka->next = tmp;
-	// 	free(tmp);
-	// }
 	return (0);
 }
